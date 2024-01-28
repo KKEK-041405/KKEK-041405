0a408b540a4227b65196872d43cad80d881586ef
KKEK <119280751+KKEK-041405@users.noreply.github.com>
2024-01-28 20:09:44 +0530

    chatbot

--- App.py
+++ App.py
@@ -1,11 +1,69 @@
 
+import PIL.Image
+import gradio as gr
+import base64
+import time
+import os
 import google.generativeai as genai
 
+# Set Google API key 
+os.environ['GOOGLE_API_KEY'] = "AIzaSyDiTTQcEtzi-QeksbGYJCJAL5Fsq9j1pVU"
+genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
 
-genai.configure(api_key="AIzaSyDiTTQcEtzi-QeksbGYJCJAL5Fsq9j1pVU")
-model = genai.GenerativeModel('gemini-pro')
+# Create the Model
+txt_model = genai.GenerativeModel('gemini-pro')
+vis_model = genai.GenerativeModel('gemini-pro-vision')
 
-prompt = input("Enter the prompt ")
-response = model.generate_content(prompt)
+# Image to Base 64 Converter
+def image_to_base64(image_path):
+    with open(image_path, 'rb') as img:
+        encoded_string = base64.b64encode(img.read())
+    return encoded_string.decode('utf-8')
 
-print(response.image)
+# Function that takes User Inputs and displays it on ChatUI
+def query_message(history,txt,img):
+    if not img:
+        history += [(txt,None)]
+        return history
+    base64 = image_to_base64(img)
+    data_url = f"data:image/jpeg;base64,{base64}"
+    history += [(f"{txt} ![]({data_url})", None)]
+    return history
+
+# Function that takes User Inputs, generates Response and displays on Chat UI
+def llm_response(history,text,img):
+    if not img:
+        response = txt_model.generate_content(text)
+        history += [(None,response.text)]
+        return history
+
+    else:
+        img = PIL.Image.open(img)
+        response = vis_model.generate_content([text,img])
+        history += [(None,response.text)]
+        return history
+
+# Interface Code
+with gr.Blocks() as app:
+    with gr.Row():
+        image_box = gr.Image(type="filepath")
+    
+        chatbot = gr.Chatbot(
+            scale = 2,
+            height=750
+        )
+    text_box = gr.Textbox(
+            placeholder="Enter text and press enter, or upload an image",
+            container=False,
+        )
+
+    btn = gr.Button("Submit")
+    clicked = btn.click(query_message,
+                        [chatbot,text_box,image_box],
+                        chatbot
+                        ).then(llm_response,
+                                [chatbot,text_box,image_box],
+                                chatbot
+                                )
+app.queue()
+app.launch(debug=True)
---
+++ public/ai.js
@@ -0,0 +1,81 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvGtuafftoof9UAs9xVekxE3ktw2RIVlk",
+    authDomain: "kkek-ef277.firebaseapp.com",
+    projectId: "kkek-ef277",
+    storageBucket: "kkek-ef277.appspot.com",
+    messagingSenderId: "1098048921319",
+    appId: "1:1098048921319:web:e30ed202c9ae7660978c42",
+    measurementId: "G-RFKMFC9QXN"
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);
+// import { connectFirestoreEmulator } from "firebase/firestore";
+
+// connectFirestoreEmulator(db, '127.0.0.1', 8080);
+
+const docRef = doc(db, "KKEK", "API");
+const docSnap = await getDoc(docRef);
+
+const API = docSnap.data().Gemini;
+// Access your API key (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(API);
+
+var History = JSON.parse('[]');
+console.log(History);
+
+
+async function run() {
+    // For text-only input, use the gemini-pro model
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+    prompt = "Write a story about a magic backpack.";
+
+    if (document.getElementById("query").value != "") {
+      prompt = document.getElementById("query").value;    
+      
+    }
+    else{return;}
+
+    console.log(prompt)
+    const responseEle = document.getElementById("response");
+    
+    const chat = model.startChat({
+        history: History,
+        generationConfig: {
+          maxOutputTokens: 100,
+        },
+      });
+    
+    responseEle.innerHTML = "Generating '"+prompt.toString()+"'";
+    
+    try {
+        const result = await chat.sendMessage(prompt);
+        const response = await result.response;
+        
+        document.getElementById("response").innerHTML = response.text();
+        History[History.length] = {"role":"user","parts":prompt};
+        History[History.length] = {"role":"model","parts":response.text()};
+        console.log(History);       
+        
+    } catch (error) {
+        responseEle.innerHTML = error.toString();
+    }
+
+    if (responseEle.innerHTML == "Generating '"+prompt.toString()+"'"){
+        responseEle.innerHTML = "due to safty issue....<br>your request is not processed..<br>pls try something else";
+    }
+    else{
+        console.log((responseEle.innerHTML == "Generating '"+prompt.toString()+"'"))
+    }
+}
+document.getElementById("submit").addEventListener("click",run);
+  
